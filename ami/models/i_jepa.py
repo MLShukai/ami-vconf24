f21a1aaaa66cd70721885c831556cf595327a6c9
zassou65535 <gravity@kuf.biglobe.ne.jp>
2024-08-01 15:29:32 +0900

    match文の導入

--- ami/models/i_jepa.py
+++ ami/models/i_jepa.py
@@ -387,18 +387,18 @@ class VisionTransformerEncoder(nn.Module):
             rescale(layer.mlp.fc2.weight.data, layer_id)
 
     def _init_weights(self, m: nn.Module) -> None:
-    match m:
-        case nn.Linear():
-            trunc_normal_(m.weight, std=self.init_std)
-            if m.bias is not None:
+        match m:
+            case nn.Linear():
+                trunc_normal_(m.weight, std=self.init_std)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            case nn.LayerNorm():
                 nn.init.constant_(m.bias, 0)
-        case nn.LayerNorm():
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-        case nn.Conv2d():
-            trunc_normal_(m.weight, std=self.init_std)
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
+            case nn.Conv2d():
+                trunc_normal_(m.weight, std=self.init_std)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
 
     def forward(
         self, images: torch.Tensor, masks_for_context_encoder: Optional[list[torch.Tensor]] = None
@@ -561,17 +561,18 @@ class VisionTransformerPredictor(nn.Module):
             rescale(layer.mlp.fc2.weight.data, layer_id + 1)
 
     def _init_weights(self, m: nn.Module) -> None:
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=self.init_std)
-            if isinstance(m, nn.Linear) and m.bias is not None:
+        match m:
+            case nn.Linear():
+                trunc_normal_(m.weight, std=self.init_std)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            case nn.LayerNorm():
                 nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-        elif isinstance(m, nn.Conv2d):
-            trunc_normal_(m.weight, std=self.init_std)
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
+            case nn.Conv2d():
+                trunc_normal_(m.weight, std=self.init_std)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
 
     def forward(
         self,
