c04616082729804d87fae4d973fbf642f1f39e02
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-09-24 08:40:26 +0900

    ResNetConv2dを追加

--- ami/models/components/resnet.py
+++ ami/models/components/resnet.py
@@ -15,3 +15,23 @@ class ResNetFF(nn.Module):
             x = ff(x)
             x = x + x_
         return x
+
+
+class ResNetConv2d(nn.Module):
+    def __init__(self, dim: int, dim_hidden: int, depth: int, activation: nn.Module = nn.ReLU()) -> None:
+        super().__init__()
+        self.conv_list = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.Conv2d(dim, dim_hidden, 3, padding=1), activation, nn.Conv2d(dim_hidden, dim, 3, padding=1)
+                )
+                for _ in range(depth)
+            ]
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        for conv in self.conv_list:
+            x_ = x
+            x = conv(x)
+            x = x + x_
+        return x
--- tests/models/components/test_resnet.py
+++ tests/models/components/test_resnet.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from ami.models.components.resnet import ResNetFF
+from ami.models.components.resnet import ResNetConv2d, ResNetFF
 
 
 class TestResNetFF:
@@ -22,3 +22,24 @@ class TestResNetFF:
         x = torch.randn(batch, dim)
         x = mod(x)
         assert x.shape == (batch, dim)
+
+
+class TestResNetConv2d:
+    @pytest.mark.parametrize(
+        """
+        batch,
+        dim,
+        dim_hidden,
+        depth,
+        size,
+        """,
+        [
+            (3, 128, 256, 4, 128),
+            (6, 28, 56, 2, 64),
+        ],
+    )
+    def test_resnet_conv2d(self, batch, dim, dim_hidden, depth, size):
+        mod = ResNetConv2d(dim, dim_hidden, depth)
+        x = torch.randn(batch, dim, size, size)
+        x = mod(x)
+        assert x.shape == (batch, dim, size, size)
