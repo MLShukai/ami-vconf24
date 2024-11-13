4b020044e5095c5e49dd1bf985e7691fd4547482
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-11-13 15:40:13 +0900

    testsとsamplesを削除

--- samples/web_api_demo.py
+++
@@ -1,7 +0,0 @@
-from ami.threads.thread_control import ThreadController, ThreadControllerStatus
-from ami.threads.web_api_handler import WebApiHandler
-
-controller = ThreadController()
-status = ThreadControllerStatus(controller)
-handler = WebApiHandler(status)
-handler.run()
--- tests/test_version.py
+++
@@ -1,5 +0,0 @@
-from ami import __version__
-
-
-def test_version() -> None:
-    __version__ == "2024.05.0"
